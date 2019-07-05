9bbcf4c07afc44c2b05f202c66d8dddd36cba00c
David Brazel <dbrazel@users.noreply.github.com>
2019-07-05 21:36:38 +0100

    now generating population pyramids for all countries

--- src/animate_projections.R
+++ src/animate_projections.R
@@ -9,20 +9,27 @@ library(stringr)
 library(forcats)
 library(bayesPop)
 
+# These are the converged simulations provided by Hana Ševčíková
 tfr.dir <- 'sim20190525'
 e0.dir <- 'sim20190528'
+
 pop.dir <- 'full_pop_sim'
 
-pop.pred <- pop.predict(
-  end.year = 2100, start.year = 1950, present.year = 2020,
-  wpp.year = 2019, output.dir = pop.dir, nr.traj = 1000,
-  inputs = list(tfr.sim.dir = tfr.dir,
-                e0F.sim.dir = e0.dir, 
-                e0M.sim.dir = "joint_"),
-  ## optionally run in parallel:
-  parallel = TRUE,
-  verbose = T
-)
+# To generate the total population trajectories:
+# pop.pred <- pop.predict(
+#   end.year = 2100, start.year = 1950, present.year = 2020,
+#   wpp.year = 2019, output.dir = pop.dir, nr.traj = 1000,
+#   inputs = list(tfr.sim.dir = tfr.dir,
+#                 e0F.sim.dir = e0.dir, 
+#                 e0M.sim.dir = "joint_"),
+#   ## optionally run in parallel:
+#   parallel = TRUE,
+#   verbose = T
+# )
+
+# Load the total population trajectories
+
+pop.pred <- get.pop.prediction(pop.dir)
 
 years <- pop.pred$proj.years.pop
 countries <- pop.pred$countries$name %>% as.character()
@@ -67,23 +74,48 @@ medians <- pmap(
 
 # ggplot2 will follow the factor order on the y-axis
 medians$age <- factor(medians$age, levels = medians[1:27, 'age'])
-medians$sex <- fct_recode(medians$sex, Female = 'female', Male = 'male')
+medians$sex <- fct_recode(medians$sex, Male = 'male', Female = 'female') %>%
+  fct_relevel('Male', 'Female')
 
-anim <- ggplot(medians, aes(age, pop, fill = sex, group = age)) + 
-  geom_bar(stat = 'identity', alpha = 0.8) +
-  scale_y_continuous(labels = function(x) as.character(abs(x))) +
-  coord_flip() +
-  facet_wrap(~ country, ncol = 6, scales = 'free_x') +
-  scale_fill_brewer(palette = 'Dark2') +
-  transition_states(year, transition_length = 0.5) +
-  shadow_trail(distance = 1, alpha = 0.5) + # Show the first value in the background
-  labs(
-    title = 'Median population projections to 2100',
-    subtitle = 'Year: {closest_state}',
-    caption = 'Visualization: David Brazel\nData: bayesPop Team',
-    x = 'Age',
-    y = 'Population') +
-  theme(panel.spacing = unit(2, "lines"), legend.title = element_blank())
+# Given a dataframe, render an animation
+make_animation <- function(df) {
+  anim <- droplevels(df) %>% # Remove unused countries
+    ggplot(aes(age, pop, fill = sex, group = age)) +
+    geom_bar(stat = 'identity', alpha = 0.8) +
+    scale_y_continuous(labels = function(x) as.character(abs(x))) +
+    coord_flip() +
+    facet_wrap(~ country, ncol = 5, scales = 'free_x') +
+    scale_fill_brewer('Sex', palette = 'Dark2') +
+    transition_states(year, transition_length = 0.5) +
+    shadow_trail(distance = 1, alpha = 0.5) + # Show the first value in the background
+    labs(
+      title = 'Median population projections to 2100',
+      subtitle = 'Year: {closest_state}',
+      caption = 'Visualization: David Brazel\nData: Hana Ševčíková and the bayesPop team',
+      x = 'Age',
+      y = 'Population') +
+    theme(legend.title = element_blank()) +
+    theme_cowplot()
+  
+  return(animate(anim, height = 922, width = 1229, nframes = 150))
+}
 
-animated <- animate(anim, height = 800, width = 1320, nframes = 150)
-anim_save('pop_pyramids.gif')
+# Sort the vector of countries alphabetically and split it into chunks of 10 countries
+countries <- sort(countries)
+
+# Make and save an animation for each chunk
+split_countries <- split(countries, ceiling(seq_along(countries) / 10))
+
+write_animation <- function(countries, df) {
+  df <- filter(df, country %in% countries)
+  
+  first <- countries[1]
+  last <- countries[length(countries)]
+  
+  anim <- make_animation(df)
+  
+  fname <- paste0('pop_pyramid_', first, '_to_', last, '.gif')
+  anim_save(fname, anim)
+}
+
+walk(split_countries, write_animation, df = medians)
