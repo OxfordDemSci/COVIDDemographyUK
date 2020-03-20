54c667564482e4f94562ba2dc5ae6b5272e7f218
Mark Verhagen <markdverhagen@gmail.com>
2020-03-20 07:37:43 +0100

    include code for LSOA-level county plots

--- src/graphing_UK.R
+++ src/graphing_UK.R
@@ -8,20 +8,15 @@ setwd("")
 
 # Load packages
 packages <- c("tidyverse", "tidycensus", "magrittr", "readxl", "sp", "gpclib", "maptools", "spdep",
-              "maptools", "RColorBrewer", "lattice", "gridExtra", "sf", "reshape2")
+              "maptools", "RColorBrewer", "lattice", "gridExtra", "sf", "reshape2", "spData", "rgeos")
 lapply(packages, require, character.only = TRUE)
 
 # Load functions
-source("graphing_functions.R")
+source("src/graphing_functions.R")
 
 # Load Data
-# agg_uk <- readRDS("data/final/UK_census_rates.rds")
 agg_uk_h <- readRDS("data/final/UK_2018_all_rates_01.rds")  # Data at 1% infection uniform
 agg_uk_h_0025 <- readRDS("data/final/UK_2018_all_rates_0025.rds")  # Data at 0.25% infection uniform
-
-# Capacity data for hospital beds (Region and Ceremonial County level)
-hospital_region <- read_region_hospital()
-hospital_ccounty <- read_county_hospital()
 
 # Crosswalk from LDA to Region
 cw_lda_region <- read.csv("data/geo_crosswalks/CW_LDA_Region.csv")  # this crosswalk is LDA to Region (we have both LSOA and LDA in the raw data)
@@ -34,8 +29,14 @@ ccounty_shape <- sf::st_read("shapefiles/UK/CCounties/Boundary-line-ceremonial-c
   mutate(NAME = gsub(" Cer", "", NAME),
          NAME = gsub("\\&", "and", NAME),
          NAME = toupper(NAME))
-# lsoa_shape <- sf::st_read("shapefiles/UK/Lower_Layer_Super_Output_Areas_December_2011_Boundaries_EW_BFC.shp") %>%
-#   merge.data.frame(cw_lsoa_ccounty[, c("LSOA11CD", "NAME")], by="LSOA11CD")  # Not necessary yet
+lsoa_shape <- sf::st_read("shapefiles/UK/Lower_Layer_Super_Output_Areas_December_2011_Boundaries_EW_BFC.shp") %>%
+  merge.data.frame(cw_lsoa_ccounty[, c("LSOA11CD", "NAME")], by="LSOA11CD")  # Not necessary yet
+
+# Capacity data for hospital beds (Region and Ceremonial County level)
+hospital_region <- read_region_hospital()
+hospital_ccounty <- rbind(read_county_hospital(), read_wales_hospital(ccounty_shape)) %>%
+  mutate(general_cap = as.numeric(general_cap),
+         acute_cap = as.numeric(acute_cap))
 
 # Read the original census file and clean the Local Authority District names to 2019 versions
 uk <- read.csv("data/census UK/UK_2018_all.csv") %>%
@@ -45,9 +46,9 @@ uk <- read.csv("data/census UK/UK_2018_all.csv") %>%
 # Create a region and county aggregated count of fatalities, hospitalizations etc.
 region_df <- regional_agg(agg_uk_h, uk, cw_lda_region, hospital_region)
 ccounty_df <- county_agg(agg_uk_h, uk, cw_lsoa_ccounty, hospital_ccounty)
-# lsoa_df <- agg_uk_h %>%
-#   group_by(AreaCodes) %>%
-#   summarise_all(list(sum))
+lsoa_df <- agg_uk_h %>%
+  group_by(AreaCodes) %>%
+  summarise_all(list(sum))
 
 # Same for 0.25% infection  
 region_df_0025 <- regional_agg(agg_uk_h_0025, uk, cw_lda_region, hospital_region)
@@ -92,8 +93,44 @@ agg_ccounty_shape <- sp::merge(ccounty_df, ccounty_shape, by.x="CCTY19NM", by.y=
          abs_excess_demand_hosp = (hospitalizations - general_cap) * 1000 / pop,
          abs_excess_demand_hosp_acute = (hospitalizations_acute - acute_cap) * 1000 / pop)
 
+agg_lsoa_shape <- sp::merge(lsoa_df, lsoa_shape, by.x="AreaCodes", by.y="LSOA11CD", all.x=T) %>%
+  mutate(pc_hosp = hospitalization / value * 1000,
+         pc_hosp_acute = hospitalization_acute / value * 1000,
+         pc_fatailties = fatalities / value * 1000)
+
+## ---- LSOA Specific Graphs ---- ##
+
+tiff("figs/capacity_plots/hospitalization_Powys.tiff", units = 'in', width=10, height = 8, res = 100)
+ggplot(data = agg_lsoa_shape[agg_lsoa_shape$NAME == "Powys", ], aes(fill = pc_hosp, geometry = geometry)) + geom_sf(color=NA) +
+  theme_linedraw() + ggtitle(paste0("Expected hospitalizations (per 1,000), Powys")) +
+  scale_fill_distiller(palette = 'PRGn', direction=-1) + theme(axis.text.x = element_blank(), axis.text.y = element_blank()) +
+  labs(caption='Source: Leverhume Center for Demographic Science. Assuming 1% infection.', fill = "Hospitalization per 1,000")
+dev.off()
+
+
+tiff("figs/capacity_plots/hospitalization_Cumbria.tiff", units = 'in', width=10, height = 8, res = 100)
+ggplot(data = agg_lsoa_shape[agg_lsoa_shape$NAME == "Cumbria", ], aes(fill = pc_hosp, geometry = geometry)) + geom_sf(color=NA) +
+  theme_linedraw() + ggtitle(paste0("Expected hospitalizations (per 1,000), Cumbria")) +
+  scale_fill_distiller(palette = 'PRGn', direction=-1) + theme(axis.text.x = element_blank(), axis.text.y = element_blank()) +
+  labs(caption='Source: Leverhume Center for Demographic Science. Assuming 1% infection.', fill = "Hospitalization per 1,000")
+dev.off()
+
+tiff("figs/capacity_plots/hospitalization_London.tiff", units = 'in', width=10, height = 8, res = 100)
+ggplot(data = agg_lsoa_shape[grepl("London", agg_lsoa_shape$NAME), ], aes(fill = pc_hosp, geometry = geometry)) + geom_sf(color=NA) +
+  theme_linedraw() + ggtitle(paste0("Expected hospitalizations (per 1,000), London")) +
+  scale_fill_distiller(palette = 'PRGn', direction=-1) + theme(axis.text.x = element_blank(), axis.text.y = element_blank()) +
+  labs(caption='Source: Leverhume Center for Demographic Science. Assuming 1% infection.', fill = "Hospitalization per 1,000")
+dev.off()
+
+tiff("figs/capacity_plots/hospitalization_Cornwall.tiff", units = 'in', width=10, height = 8, res = 100)
+ggplot(data = agg_lsoa_shape[agg_lsoa_shape$NAME == "Cornwall", ], aes(fill = pc_hosp, geometry = geometry)) + geom_sf(color=NA) +
+  theme_linedraw() + ggtitle(paste0("Expected hospitalizations (per 1,000), Cornwall")) +
+  scale_fill_distiller(palette = 'PRGn', direction=-1) + theme(axis.text.x = element_blank(), axis.text.y = element_blank()) +
+  labs(caption='Source: Leverhume Center for Demographic Science. Assuming 1% infection.', fill = "Hospitalization per 1,000")
+dev.off()
+
 ## ------ Capacity plots ------ ##
-png("figs/capacity_plots/region_general_capacity.tiff")
+tiff("figs/capacity_plots/region_general_capacity.tiff")
 ggplot(data = agg_region_shape, aes(fill = pc_capacity, geometry = geometry)) + geom_sf() +
   theme_linedraw() + ggtitle(paste0("Per capita hospital bed capacity (per 1,000), England. Regional level")) +
   scale_fill_distiller(palette = 'Blues', direction=1) + theme(axis.text.x = element_blank(), axis.text.y = element_blank()) +
@@ -105,18 +142,16 @@ ggplot(data = agg_region_shape, aes(fill = pc_capacity_acute, geometry = geometr
   theme_linedraw() + ggtitle(paste0("Per capita acute care bed capacity (per 1,000), England. Regional level")) +
   scale_fill_distiller(palette = 'Reds', direction=1) + theme(axis.text.x = element_blank(), axis.text.y = element_blank()) +
   labs(caption='Source: Mid-18 Pop. Estimates, Office for National Statistics and NHS England', fill = "Beds per 1,000")
-
 dev.off()
 
-
-png("figs/capacity_plots/ccounty_general_capacity.tiff")
+tiff("figs/capacity_plots/ccounty_general_capacity.tiff", units = 'in', width=10, height = 8, res = 100)
 ggplot(data = agg_ccounty_shape, aes(fill = pc_capacity, geometry = geometry)) + geom_sf() +
   theme_linedraw() + ggtitle(paste0("Per capita hospital bed capacity (per 1,000), England. Ceremonial County level")) +
   scale_fill_distiller(palette = 'Blues', direction=1) + theme(axis.text.x = element_blank(), axis.text.y = element_blank()) +
   labs(caption='Source: Mid-18 Pop. Estimates, Office for National Statistics and NHS England', fill = "Beds per 1,000")
 dev.off()
 
-png("figs/capacity_plots/ccounty_acute_capacity.tiff")
+tiff("figs/capacity_plots/ccounty_acute_capacity.tiff", units = 'in', width=10, height = 8, res = 100)
 ggplot(data = agg_ccounty_shape, aes(fill = pc_capacity_acute, geometry = geometry)) + geom_sf() +
   theme_linedraw() + ggtitle(paste0("Per capita acute care bed capacity (per 1,000), England. Ceremonial County level")) +
   scale_fill_distiller(palette = 'Reds', direction=1) + theme(axis.text.x = element_blank(), axis.text.y = element_blank()) +
